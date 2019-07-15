a76b18d2ca14d57676cdb498593c5a6f4ba7da55
Nadezhda Komaritckaia <komaritskaya@Nadezhdas-MacBook-Pro.local>
2019-07-16 01:48:59 +0300

    added some slick items

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $(".videos__carousel").slick({
+    dots: false,
+    infinite: true,
+    swipe: true,
+    autoplay: false,
+    speed: 500,
+    arrows: false,
+    adaptiveHeight: true
+  });
+});
